700c19fe1f14dd46deca89460c749222530ce772
ngxrx <91258275+ngxrx@users.noreply.github.com>
2022-08-21 20:37:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
