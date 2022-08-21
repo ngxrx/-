dd47dae8957b58587bb39999d4b96a14065dbf1f
ngxrx <91258275+ngxrx@users.noreply.github.com>
2022-08-21 20:51:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-
+birthday = os.environ['BIRTHDAY']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-birthday = os.environ['BIRTHDAY']
+
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
