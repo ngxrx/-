de1e9081d43e8171230807705160139f5cf79eea
ngxrx <91258275+ngxrx@users.noreply.github.com>
2022-09-15 14:47:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
-  return (next - today).days
+  return (next - today).days+1
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
